538f9c587563f35b639880a4965447eca2edbc1b
ludonglue <ludonglue@163.com>
2017-09-11 10:10:03 +0800

    哈哈哈

--- src/main/java/com/example/demo/testme.java
+++ src/main/java/com/example/demo/testme.java
@@ -4,4 +4,7 @@ package com.example.demo;
  * Created by ludonglue on 2017/9/11.
  */
 public class testme {
+    public static void main(String[] argc){
+        System.out.println("aaa");
+    }
 }
